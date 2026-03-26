728096128cd60fc7c7e16a8b5938b5235fda48d9
Noé Steiner <noe.steiner@infomaniak.com>
2026-03-26 07:48:26 +0100

    feat: switch to postgres db

--- internal/config/env.go
+++ internal/config/env.go
@@ -7,11 +7,20 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type DatabaseConfig struct {
+	Host     string `env:"HOST" envDefault:"localhost"`
+	Port     string `env:"PORT" envDefault:"5432"`
+	User     string `env:"USER" envDefault:"citadelle"`
+	Password string `env:"PASSWORD,required"`
+	Name     string `env:"NAME" envDefault:"citadelle"`
+	SSLMode  string `env:"SSL_MODE" envDefault:"disable"`
+}
+
 type Config struct {
-	Port           string   `env:"PORT" envDefault:"8080"`
-	DBPath         string   `env:"DB_PATH" envDefault:"citadelle.db"`
-	JWTSecret      string   `env:"JWT_SECRET,required"`
-	AllowedOrigins []string `env:"ALLOWED_ORIGINS,required" envSeparator:","`
+	Port           string         `env:"PORT" envDefault:"8080"`
+	Database       DatabaseConfig `envPrefix:"DB_"`
+	JWTSecret      string         `env:"JWT_SECRET,required"`
+	AllowedOrigins []string       `env:"ALLOWED_ORIGINS,required" envSeparator:","`
 }
 
 func Load() (*Config, error) {
