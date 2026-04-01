23685268fe80c767653c056139ba253db2aa7ff7
Noé Steiner <noe.steiner@infomaniak.com>
2026-04-01 21:33:35 +0200

    feat: pagination types

--- internal/handlers/types.go
+++ internal/handlers/types.go
@@ -1,5 +1,7 @@
 package handlers
 
+import "github.com/go-fuego/fuego"
+
 type ApiResponse[T any] struct {
 	Data    *T     `json:"data"`
 	Message string `json:"message"`
@@ -11,4 +13,41 @@ func NewApiResponse[T any](data *T, message string) (*ApiResponse[T], error) {
 
 func NewErrorResponse[T any](err error) (*ApiResponse[T], error) {
 	return nil, err
+}
+
+type PaginatedApiResponse[T any] struct {
+	Data    []T    `json:"data"`
+	Message string `json:"message"`
+	Page    uint64 `json:"page"`
+	PerPage uint64 `json:"perPage"`
+	Total   uint64 `json:"total"`
+}
+
+func NewPaginatedApiResponse[T any](
+	data []T,
+	message string,
+) *PaginatedApiResponse[T] {
+	return &PaginatedApiResponse[T]{
+		Data:    data,
+		Message: message,
+	}
+}
+
+func PaginationParamsFromRequest[T any](c fuego.Context[T, any]) PaginationParams {
+	page := c.QueryParamInt("page")
+	perPage := c.QueryParamInt("perPage")
+	return PaginationParams{
+		Page:    uint64(page),
+		PerPage: uint64(perPage),
+	}
+}
+
+type PaginationParams struct {
+	Page    uint64
+	PerPage uint64
+}
+
+type Pagination struct {
+	PaginationParams
+	Total uint64
 }
